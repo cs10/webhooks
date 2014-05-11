4da989632403f854df5120d62d44f3a4533e6feb
Michael Ball <cycomachead@gmail.com>
2014-05-11 01:24:17 -0700

    some scripts work ish but not really.

--- gh_pull.php
+++ gh_pull.php
@@ -4,7 +4,14 @@ phpinfo();
 echo 'User: ' . get_current_user() . '<br />';
 $file = 'github.py';
 $url = "//$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-$result = exec('python ' . $file . ' ' . $url . '2>&1');
+$command = 'python ' . $file . ' "' . $url . '" 2>&1';
+echo $file;
+echo '<br />';
+echo $url;
+echo '<br />';
+echo $command;
+echo '<br />';
+$result = exec($command);
 echo $result;
 
 ?>
--- github.py
+++ github.py
@@ -1,9 +1,17 @@
 import sys, os, datetime
 
 name = "github_event_" + str(datetime.datetime.now())
-
-with open(name, "wt") as myfile:
-    os.chmod(name, 0744)
+log = "../web_hook_log.txt"
+with open(name, "w+") as myfile:
+    #os.chmod(name, 0744)
+    myfile.write('\n ')
     if len(sys.argv) > 1:
         myfile.write(sys.argv[1])
+    myfile.close()
+
+with open(log, "a") as myfile:
+    myfile.write(str(datetime.datetime.now()) + '\n\n')
+    if len(sys.argv) > 1:
+        myfile.write(sys.argv[1])
+        myfile.write('\n\n\n')
     myfile.close()
