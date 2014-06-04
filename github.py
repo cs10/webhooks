cd869995856eceb0027e4361c55561755b15679f
Michael Ball <cycomachead@gmail.com>
2014-06-04 00:48:35 -0700

    HOLY CRAP IT WORKS! now must write docs

--- github.py
+++ github.py
@@ -1,18 +1,20 @@
+print("Running Pull Script")
+
 import sys, os, datetime
 
-name = "github_event_" + str(datetime.datetime.now())
-log = "../web_hook_log.txt"
-
+name = "/home/ff/cs10/logs/wgithub_event_" + str(datetime.datetime.now()) + ".txt"
+log = "/home/ff/cs10/logs/web_hook_log.txt"
 with open(name, "w+") as myfile:
     #os.chmod(name, 0744)
-    myfile.write('\n ')
+    myfile.write('writing... \n ')
     if len(sys.argv) > 1:
         myfile.write(sys.argv[1])
     myfile.close()
 
 with open(log, "a") as myfile:
     myfile.write(str(datetime.datetime.now()) + '\n\n')
+    myfile.write("Hmmmm.... \n")
     if len(sys.argv) > 1:
-        myfile.write(sys.argv[1])
+    	myfile.write(sys.argv[1])
         myfile.write('\n\n\n')
     myfile.close()
