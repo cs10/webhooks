eab52ac8d7a42d9eefd240d602342686c1cd49d7
Michael Ball <cycomachead@gmail.com>
2014-06-26 12:27:34 -0700

    progess on PHP script; add url py for labs -- this needs a better location

--- gh.php
+++ gh.php
@@ -3,33 +3,36 @@
 $dir = '/home/ff/cs10/github/';
 // System python command (python isn't in PHP user path)
 $python = '/usr/sww/bin/python3';
-$log = $dir . 'php_log.txt';
+$logFile = $dir . 'php_log.txt';
 // Script to execute
 $file = $dir . 'github.py';
 // Logging shortcuts
 $nl   = '
 ';
 $sep = '==================================================';
-$event = $_POST['X-Github-Event'];
+$event = $_POST['X-GitHub-Event']; // note capitalization!
 // Command to run
-// MAKE SURE TO INCLUDE '' AROUND THE POST DATA
+// MAKE SURE TO ENCODE THE POST DATA! -- put it in '' as well.
 // Command Captures stderr and stdout
-$command = $python . ' ' . $file . ' \'' . $HTTP_RAW_POST_DATA . '\' \'' . $event . '\' 2>&1';
-// Logging stuffs
-file_put_contents($log, $nl . $sep . $nl, FILE_APPEND);
-file_put_contents($log, 'WEB HOOK EXEC  @ ' . date("Y-m-d H:i:s") . $nl, FILE_APPEND);
+$command = $python . ' ' . $file . ' \'' . rawurlencode($HTTP_RAW_POST_DATA) . '\' \'' . $event . '\' 2>&1';
+// Pre-Exec Logging stuffs
+// Log to File and Echo for GH output.
+$logData = $nl . $sep . $nl;
+$logData = $logData . 'WEB HOOK EXEC  @ ' . date("Y-m-d H:i:s") . $nl;
+$logData = $logData . 'WEB HOOK EVENT - ' . $event . $nl;
 // TODO: Log Repo Name, Account
-file_put_contents($log, 'WEB HOOK EVENT - ' . $event . $nl, FILE_APPEND);
+file_put_contents($logFile, $logData, FILE_APPEND);
+// TEST -- Log Python User to GH
+$py = exec($python . ' -c \'import os; print(os.system("whoami"))\'');
+echo($py);
+echo($logData);
+var_dump($_POST);
+echo('COMMAND:  ' . $nl . $command . $nl . $sep . $nl);
 // Run command, save output and log it.
 $result = exec($command);
-file_put_contents($log, 'RESULT:  ' . $result, FILE_APPEND);
-file_put_contents($log, $nl . $sep . $nl, FILE_APPEND);
 // Send some data back about the results
-// Mostly helpful for debugging on GH
-// TODO: Send appropriate status code
-HttpResponse::status(200);
-HttpResponse::setContentType('text/xml');
-HttpResponse::setData('WEB HOOK EXEC FINISHED:' . $nl . $result);
-HttpResponse::send();
+echo('RESULT:  ' . $result);
+file_put_contents($logFile, 'RESULT:  ' . $result, FILE_APPEND);
+file_put_contents($logFile, $nl . $sep . $nl, FILE_APPEND);
 
 ?>
---
+++ scripts/bjcr_urls.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python
+
+# This fixes the directories for running labs on the CS10 website
+# This script is largely a terrible hack and shouldn't be used for long.
+
+import os
+import fnmatch
+def findReplace(directory, find, replace, filePattern):
+    for path, dirs, files in os.walk(os.path.abspath(directory)):
+        for filename in fnmatch.filter(files, filePattern):
+            filepath = os.path.join(path, filename)
+            with open(filepath) as f:
+                s = f.read()
+            s = s.replace(find, replace)
+            with open(filepath, "w") as f:
+                f.write(s)
+
+labs="/home/ff/cs10/public_html/labs"
+michael="/Volumes/Michael/Users/Michael/Dropbox/Projects/bjc-r copy"
+findReplace(labs, "\"/bjc-r", "\"/~cs10/labs", "*.html")
+findReplace(labs, "/bjc-r", "/~cs10/labs", "*.topic")
+findReplace(labs, "/bjc-r", "/~cs10/labs", "*.js")
+
